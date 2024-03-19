3769152273be7e8f3e87b8f25942f4f15df332f1
Stevan <stevan14padilha@gmail.com>
2024-03-19 09:41:55 -0300

    feat: creating the snake, body and head

--- snake.py
+++ snake.py
@@ -4,24 +4,32 @@ import curses
 def game_loop(window):
     # Setup inicial
     curses.curs_set(0)
-    snake = [10, 15]
+    snake = [
+        [10, 15],
+        [9, 15],
+    ]
     current_direction = curses.KEY_DOWN
 
     while True:
         draw_screen(window=window)
-        draw_actor(actor=snake, window=window)
+        draw_snake(snake=snake, window=window)
 
         direction = get_new_direction(window=window, timeout=1000)
 
         if direction is None:
             direction = current_direction
 
-        move_actor(actor=snake, direction=direction)
+        move_snake(snake=snake, direction=direction)
 
-        if actor_hit_border(actor=snake, window=window):
+        if snake_hit_border(snake=snake, window=window):
             return
 
         current_direction = direction
+
+
+def snake_hit_border(snake, window):
+    head = snake[0]
+    return actor_hit_border(actor=head, window=window)
 
 
 def draw_screen(window):
@@ -29,18 +37,31 @@ def draw_screen(window):
     window.border(0)
 
 
-def draw_actor(actor, window):
-    window.addch(actor[0], actor[1], curses.ACS_DIAMOND)
+def draw_snake(snake, window):
+    head = snake[0]
+    draw_actor(actor=head, window=window, char="♛")
+    body = snake[1:]
+    for body_part in body:
+        draw_actor(actor=body_part, window=window, char="✱")
+
+
+def draw_actor(actor, window, char):
+    window.addch(actor[0], actor[1], char)
 
 
 def get_new_direction(window, timeout):
     window.timeout(timeout)
     direction = window.getch()
-
     if direction in [curses.KEY_UP, curses.KEY_LEFT, curses.KEY_DOWN, curses.KEY_RIGHT]:
         return direction
-
     return None
+
+
+def move_snake(snake, direction):
+    head = snake[0].copy()
+    move_actor(actor=head, direction=direction)
+    snake.insert(0, head)
+    snake.pop()
 
 
 def move_actor(actor, direction):
@@ -57,7 +78,6 @@ def move_actor(actor, direction):
 
 def actor_hit_border(actor, window):
     height, width = window.getmaxyx()
-
     if (actor[0] <= 0) or (actor[0] >= height - 1):
         return True
     if (actor[1] <= 0) or (actor[1] >= width - 1):
